f819bc79f66a36604cc710f285c01ebdf1e96232
zhenj579 <50351080+zhenj579@users.noreply.github.com>
2025-06-29 00:41:32 -0400

    add desktop components

---
+++ src/Desktop.tsx
@@ -0,0 +1,10 @@
+import './Desktop.css'
+import { DesktopIcon } from './DesktopIcon'
+
+export function Desktop() {
+    return (
+        <div className='icon-container'>
+            <DesktopIcon/>
+        </div>
+    )
+}
